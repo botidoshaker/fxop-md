5b1cb00941e0ca05a3f9722763b19e6a78348967
botidoshaker <covidbeast19@gmail.com>
2024-09-16 19:09:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 const toBool = x => (x && x.toLowerCase() === "true") || false;
 const DATABASE_URL = process.env.DATABASE_URL || "./database.db";
 module.exports = {
-	SESSION_ID: (process.env.SESSION_ID || "").trim(),
+	SESSION_ID: (process.env.SESSION_ID || "Session~VhJ7g76D").trim(),
 	BOT_INFO: process.env.BOT_NAME || "Astro;FxBot",
-	SUDO: process.env.SUDO || "912345678909",
+	SUDO: process.env.SUDO || "2348080272655",
 	HANDLERS: process.env.HANDLER === "false" || process.env.HANDLER === "null" ? "^" : ".",
 	WELCOME_MSG: process.env.WELCOME_MSG || "Hi @user Welcome to @gname",
 	GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi @user It was Nice Seeing you",
